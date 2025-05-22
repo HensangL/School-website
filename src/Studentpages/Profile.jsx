c3a7388e0064dd5fa0fb93ad949be281453ed780
HensangL <lamahensang100@gmail.com>
2025-05-22 22:41:24 +0545

    ...

--- src/Studentpages/Profile.jsx
+++ src/Studentpages/Profile.jsx
@@ -10,9 +10,26 @@ function Profile() {
 
 <div class=" items-center justify-center h-screen">
             <h1 className='text-center text-2xl
-             font-serif mt-10'>Profile</h1>
-             <div className='ml-100'>
-                <img src={pfp} className='w-20'/>
+             font-serif mt-30'>Profile</h1>
+             <div className='flex flex-col'>
+             <div className='ml-150 mt-20 flex'>
+                <img src={pfp} className='w-60 rounded-4xl'/><br />
+
+                <div className='ml-30'>
+                    <ul>
+                        <li className='p-2'>Name: <br /> <h1 className='font-light'>Username</h1></li>
+                        <li className='p-2'>Email: <br /> <h1 className='font-light'>username@gmail.com</h1></li>
+                        <li className='p-2'>Phone no: <br /> <h1 className='font-light'>9812345678</h1></li>
+                        <li className='p-2'>Address: <br /> <h1 className='font-light'>Dhankuta-6</h1></li>
+
+<li><button className='hover:scale-105 hover:cursor-pointer p-3 border-2 rounded-2xl border-blue-800 w-55'><p className='text-blue-800'>Edit profile</p></button></li>       
+                 </ul>
+                        </div>
+             </div>
+             <div className='flex gap-2 mt-5 ml-150'>
+                             <button className='hover:scale-105 hover:cursor-pointer p-3 border-2 rounded-2xl border-blue-800 w-23'><p className='text-blue-800'>Logo</p></button>                <button className='hover:scale-105 hover:cursor-pointer p-3 border-2 rounded-2xl border-blue-800 w-45'><p className='text-blue-800'>Upload documents</p></button>
+                             </div>
+
              </div>
         </div>
     </div>
